70d74181900e5d1af1acd25ff70983d9590e3eb4
luanau <luanau@gmail.com>
2016-05-22 16:10:20 +0100

    Split into 3 components

--- src/index.jsx
+++ src/index.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Voting from './components/Voting';
 
-const pair = ['Trainspotting', '29 Days Later'];
+const pair = ['Trainspotting', '31 Days Later'];
 
 ReactDOM.render(
   <Voting pair={pair} />,
