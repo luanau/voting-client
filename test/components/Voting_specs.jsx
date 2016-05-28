5d9e80add9ae2bd9226eea09d275738de94d5af5
luanau <luanau@gmail.com>
2016-05-28 09:49:46 +0100

    Add test 'adds label to the voted entry'

--- test/components/Voting_specs.jsx
+++ test/components/Voting_specs.jsx
@@ -35,5 +35,27 @@ describe('Voting', () => {
 
     expect(votedWith).to.equal('28 Days Later');
   });
+  
+  it('disables buttons when user has voted', () => {
+    const component = renderIntoDocument(
+      <Voting pair={["Trainspotting", "28 Days Later"]}
+              hasVoted="Trainspotting" />
+    );
+    const buttons = scryRenderedDOMComponentsWithTag(component, 'button');
+
+    expect(buttons.length).to.equal(2);
+    expect(buttons[0].hasAttribute('disabled')).to.equal(true);
+    expect(buttons[1].hasAttribute('disabled')).to.equal(true);
+  });
+  
+  it('adds label to the voted entry', () => {
+    const component = renderIntoDocument(
+      <Voting pair={["Trainspotting", "28 Days Later"]}
+              hasVoted="Trainspotting" />
+    );
+    const buttons = scryRenderedDOMComponentsWithTag(component, 'button');
+
+    expect(buttons[0].textContent).to.contain('Voted');
+  });
 
 });
